13deb9e0910611df6c9c15840a5b082afcd58b3d
kovshenin <kovshenin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-25 08:03:50 +0000

    tagging 1.1, last was 1.1. too not 1.2 sorry
    git-svn-id: https://plugins.svn.wordpress.org/googl/tags/1.1@336847 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ googl.php
@@ -0,0 +1,59 @@
+<?php
+/*
+Plugin Name: Goo.gl
+Plugin URI: http://kovshenin.com/
+Description: A simple Goo.gl URL shortener for WordPress.
+Author: Konstantin Kovshenin
+Version: 1.1
+Author URI: http://kovshenin.com/
+*/
+
+function googl_shortlink($url, $post_id) {
+	global $post;
+	if (!$post_id && $post) $post_id = $post->ID;
+	
+	if ($post->post_status != 'publish')
+		return "";
+
+	$shortlink = get_post_meta($post_id, '_googl_shortlink', true);
+	if ($shortlink)
+		return $shortlink;
+	
+	$permalink = get_permalink($post_id);
+
+	$http = new WP_Http();
+	$headers = array('Content-Type' => 'application/json');
+	$result = $http->request('https://www.googleapis.com/urlshortener/v1/url', array( 'method' => 'POST', 'body' => '{"longUrl": "' . $permalink . '"}', 'headers' => $headers));
+	$result = json_decode($result['body']);
+	$shortlink = $result->id;
+	
+	if ($shortlink) {
+		add_post_meta($post_id, '_googl_shortlink', $shortlink, true);
+		return $shortlink;
+	}
+	else {
+		return $url;
+	}
+}
+
+function googl_post_columns($columns)
+{
+	$columns['shortlink'] = 'Shortlink';
+	return $columns;
+}
+
+function googl_custom_columns($column)
+{
+	global $post;
+	if ('shortlink' == $column)
+	{
+		$shorturl = wp_get_shortlink();
+		$shorturl_caption = str_replace('http://', '', $shorturl);
+		$shorturl_info = str_replace('goo.gl/', 'goo.gl/info/', $shorturl);		
+		echo "<a href='{$shorturl}'>{$shorturl_caption}</a> (<a href='{$shorturl_info}'>info</a>)";
+	}
+}
+
+add_filter('get_shortlink', 'googl_shortlink', 9, 2);
+add_action('manage_posts_custom_column', 'googl_custom_columns');
+add_filter('manage_edit-post_columns', 'googl_post_columns');
---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== Goo.gl ===
+Contributors: kovshenin
+Donate link: http://kovshenin.com/
+Tags: links, twitter, short, url, socialmedia, permalinks, redirect, trim, identi.ca, microblogging, shorturl, canonical, analytics
+Requires at least: 3.0
+Tested up to: 3.0.4
+Stable tag: 1.1
+
+Uses Google's URL Shortener API (Goo.gl) to create short links for your WordPress posts.
+
+== Description ==
+
+Google has launched a URL Shortener API (Goo.gl) - one of the fastest and most reliable URL shortners out there. This plugin creates goo.gl short URLs for your posts, which then could be retrieved using the "Get Shortlink" button in your admin UI or the `wp_get_shortlink()` WordPress function. Goo.gl links analytics also available!
+
+Requires PHP5, WP_Http (curl, external fopen, etc) and preferably HTTPS!
+
+== Installation ==
+
+1. Upload archive contents to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. You're done!
+
+== Frequently Asked Questions ==
+
+Feel free to ask ;)
+
+== Screenshots ==
+
+1. Shortened URLs
+2. Posts List and Analytics
+
+== Change log ==
+
+= 1.1 =
+* Some code refactoring
+* Posts list now with short links
+* Links to Analytics
+
+= 1.0 =
+* Hurray
